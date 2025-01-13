e20f517848af10b7036c0d4e5234ca2100fbe2b0
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-13 16:56:56 +0100

    [update] main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,32 @@
 const Main = () => {
     return(
-        <main>Sono il main</main>
+        <main>
+            <div className="content-placeholder">
+                --Content Goes Here--
+            </div>
+            <div className="blue-banner">
+                <div>
+                    <img src="/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div>
+                    <img src="/img/buy-comics-merchandise.png" alt="" />
+                    <p>DC MERCHANDISE</p>
+                </div>
+                <div>
+                    <img src="/img/buy-comics-subscriptions.png" alt="" />
+                    <p>SUBSCRIPTION</p>
+                </div>
+                <div>
+                    <img src="/img/buy-comics-shop-locator.png" alt="" />
+                    <p>COMIC SHOP LOCATOR</p>
+                </div>
+                <div>
+                    <img src="/img/buy-dc-power-visa.svg" alt="" />
+                    <p>DC POWER VISA</p>
+                </div>
+            </div>
+        </main>
     )
 };
 
