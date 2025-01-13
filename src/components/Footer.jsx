4293cb75d8bda99098195f83e02c19934bdac246
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-13 18:50:06 +0100

    [update] footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () =>{
     return(
-        <footer>
-            <div className="background-heroes">
-                <div className="menucont">
-                <nav>
+        <footer className="background-heroes">
+        <div className="container footerCont">
+            <div className="menucont">
+                <div>
+                    <span>DC COMICS</span>
                     <ul>
-                        <p>DC COMICS</p>
                         <li><a href="#">Characters</a></li>
                         <li><a href="#">Comics</a></li>
                         <li><a href="#">Movies</a></li>
@@ -14,13 +14,15 @@ const Footer = () =>{
                         <li><a href="#">Videos</a></li>
                         <li><a href="#">News</a></li>
                     </ul>
+                        <span>SHOP</span>
                     <ul>
-                        <p>SHOP</p>
                         <li><a href="#">Shop DC</a></li>
                         <li><a href="#">Shop DC Collectibles</a></li>
                     </ul>
+                </div>
+                <div>
+                        <span>DC</span>
                     <ul>
-                        <p>DC</p>
                         <li><a href="#">Terms of use</a></li>
                         <li><a href="#">Privacy Policy</a></li>
                         <li><a href="#">AD Choices</a></li>
@@ -33,18 +35,24 @@ const Footer = () =>{
                         <li><a href="#">Shop Help</a></li>
                         <li><a href="#">Contact US</a></li>
                     </ul>
+                </div>
+                <div>
+                        <span>SITES</span>
                     <ul>
-                        <p>SITES</p>
                         <li><a href="#">DC</a></li>
                         <li><a href="#">MAD Magazine</a></li>
                         <li><a href="#">DC Kids</a></li>
                         <li><a href="#">DC Universe</a></li>
                         <li><a href="#">DC Power Visa</a></li>
                     </ul>
-                </nav>
                 </div>
-               
+                    
             </div>
+            <div className="imageCont">
+                <img src="/img/dc-logo-bg.png" alt="" />
+            </div>
+
+        </div>
             <div className="gray-banner">
                 <button>
                     SIGN-UP NOW!
