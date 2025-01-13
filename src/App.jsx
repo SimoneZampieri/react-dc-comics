46b103196f4d3efe1737f4e81475e923f373f0fa
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-13 15:54:00 +0100

    [update] app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+
+
 const App = () =>{
   return(
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,24 @@
+import Logo from ''
+
 const Header = () => {
     return(
-        <header>sono l'header</header>
+        <header>
+            <Logo />
+            <nav>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 };
 
