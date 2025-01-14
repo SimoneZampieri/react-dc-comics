ce20e69596d1b9ba0044127191f00c70b9358013
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-14 15:58:57 +0100

    [update] app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,38 @@ const footerShop = [
   { id: 2, url: "#", text: "Shop DC Collectibles" },
 ];
 
+const footerDC = [
+  { id: 1, url: "#", text: "Therms Of Use" },
+  { id: 2, url: "#", text: "Privacy policy (New)" },
+  { id: 3, url: "#", text: "Ad Choices" },
+  { id: 4, url: "#", text: "Advertising" },
+  { id: 5, url: "#", text: "Jobs" },
+  { id: 6, url: "#", text: "Subscriptions" },
+  { id: 7, url: "#", text: "Talent Workshops" },
+  { id: 8, url: "#", text: "CPSC Certificates" },
+  { id: 9, url: "#", text: "Ratings" },
+  { id: 10, url: "#", text: "Shop Help" },
+  { id: 11, url: "#", text: "Contact Us" },
+];
+
+const footerSites = [
+  { id: 1, url: "#", text: "DC" },
+  { id: 2, url: "#", text: "MAD Magazine" },
+  { id: 3, url: "#", text: "DC Kids" },
+  { id: 4, url: "#", text: "DC Universe" },
+  { id: 5, url: "#", text: "DC Power Visa" },
+];
+
 const App = () => {
   return (
     <>
       <Header menu={links} />
       <Main />
-      <Footer />
+      <Footer
+        footerShop={footerShop}
+        footerDC={footerDC}
+        footerSites={footerSites}
+      />
     </>
   );
 };
